8b769f16b1affdc767b56bb10209eb0de429c5e9
cherry.k <cherry.k@kakaoenterprise.com>
2022-12-20 10:23:56 +0900

    feat: 데모 배포

--- src/utils/getSvgPathFromStroke.ts
+++ src/utils/getSvgPathFromStroke.ts
@@ -2,7 +2,7 @@ export function getSvgPathFromStroke(stroke: any) {
   if (!stroke.length) return "";
 
   const d = stroke.reduce(
-    (acc, [x0, y0], i, arr) => {
+    (acc: any[], [x0, y0]: any, i: number, arr: string | any[]) => {
       const [x1, y1] = arr[(i + 1) % arr.length];
       acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
       return acc;
