b9b92c4a399235373c2262b27eea264c33b30df9
cherry.k <cherry.k@kakaoenterprise.com>
2023-01-02 10:52:34 +0900

    style: 사용안하는 코드 제거

--- src/components/Canvas/Lines/Line.tsx
+++ src/components/Canvas/Lines/Line.tsx
@@ -1,7 +1,6 @@
 import { PositionType } from "@/types/post";
 import { getSvgPathFromStroke } from "@/utils/getSvgPathFromStroke";
 import { getStroke } from "perfect-freehand";
-import { forwardRef } from "react";
 
 export type Point = [number, number, number];
 
@@ -28,39 +27,34 @@ const options = {
   smoothing: 0.8,
 };
 
-const Line = forwardRef(
-  (
-    {
-      type = "LINE",
-      id,
-      position,
-      lineInfo: { style, points, isAnimated = false },
-    }: LineProps,
-    forwardRef: any
-  ) => {
-    const stroke = getStroke(points, options);
-    const pathData = getSvgPathFromStroke(stroke);
+const Line = ({
+  type = "LINE",
+  id,
+  position,
+  lineInfo: { style, points, isAnimated = false },
+}: LineProps) => {
+  const stroke = getStroke(points, options);
+  const pathData = getSvgPathFromStroke(stroke);
 
-    return (
-      <svg
-        ref={forwardRef}
-        data-id={id}
-        viewBox={`-10 -10 ${style.width} ${style.height}`}
-        style={{
-          position: "absolute",
-          left: 0,
-          top: 0,
-          width: style.width + "px",
-          height: style.height + "px",
-          transition: isAnimated ? "transform 120ms linear" : "",
-          transform: `translate(${position.x}px, ${position.y}px)`,
-        }}
-      >
-        <g>
-          <path d={pathData} />
-        </g>
-      </svg>
-    );
-  }
-);
+  return (
+    <svg
+      data-id={id}
+      viewBox={`-10 -10 ${style.width} ${style.height}`}
+      style={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: style.width + "px",
+        height: style.height + "px",
+        transition: isAnimated ? "transform 120ms linear" : "",
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+    >
+      <g>
+        <path d={pathData} />
+      </g>
+    </svg>
+  );
+};
+
 export default Line;
--- src/components/Canvas/Post/Post.tsx
+++ src/components/Canvas/Post/Post.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { PositionType, POST } from "../../../types/post";
 import useYLayers from "../../../hook/useYLayers";
 import { useCanvas } from "../../../store/canvas";
@@ -10,47 +10,44 @@ export type PostProps = {
   postInfo: POST;
 };
 
-const Post = forwardRef(
-  ({ id, type = "POST", position, postInfo }: PostProps, forwardRef: any) => {
-    const { canvas } = useCanvas();
-    const [contents, setContents] = useState(postInfo.contents);
-    const { setLayer } = useYLayers();
+const Post = ({ id, type = "POST", position, postInfo }: PostProps) => {
+  const { canvas } = useCanvas();
+  const [contents, setContents] = useState(postInfo.contents);
+  const { setLayer } = useYLayers();
 
-    useEffect(() => {
-      if (postInfo) setContents(postInfo.contents);
-    }, [postInfo.contents]);
+  useEffect(() => {
+    if (postInfo) setContents(postInfo.contents);
+  }, [postInfo.contents]);
 
-    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setContents(e.target.value);
-      setLayer({
-        id,
-        type: "POST",
-        postInfo: {
-          contents: e.target.value,
-        },
-      });
-    };
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContents(e.target.value);
+    setLayer({
+      id,
+      type: "POST",
+      postInfo: {
+        contents: e.target.value,
+      },
+    });
+  };
 
-    return (
-      <div
-        ref={forwardRef}
-        className="absolute bg-yellow-200 shadow-md top-0 left-0"
-        style={{
-          transition: postInfo.isAnimated ? "transform 50ms linear" : "",
-          transform: `translate(${position?.x || 0}px, ${position?.y || 0}px)`,
-        }}
-        data-id={id}
-      >
-        <textarea
-          placeholder="내용을 입력하세요."
-          onChange={onChange}
-          className="p-5 bg-transparent focus:outline-none placeholder:italic placeholder-gray-500 placeholder-opacity-20"
-          style={{ width: postInfo.width, height: postInfo.height }}
-          value={contents}
-          disabled={!(canvas.mode === "NONE" || canvas.mode === "SELECTION")}
-        />
-      </div>
-    );
-  }
-);
+  return (
+    <div
+      className="absolute bg-yellow-200 shadow-md top-0 left-0"
+      style={{
+        transition: postInfo.isAnimated ? "transform 50ms linear" : "",
+        transform: `translate(${position?.x || 0}px, ${position?.y || 0}px)`,
+      }}
+      data-id={id}
+    >
+      <textarea
+        placeholder="내용을 입력하세요."
+        onChange={onChange}
+        className="p-5 bg-transparent focus:outline-none placeholder:italic placeholder-gray-500 placeholder-opacity-20"
+        style={{ width: postInfo.width, height: postInfo.height }}
+        value={contents}
+        disabled={!(canvas.mode === "NONE" || canvas.mode === "SELECTION")}
+      />
+    </div>
+  );
+};
 export default Post;
